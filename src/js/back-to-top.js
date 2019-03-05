59e65bc5c6e371d05bb181c808a278270f875d84
qi27r6yh <mr.nikswim@mail.ru>
2019-03-05 21:03:42 +0700

    Добавлен slick-слайдер в секции Portfolio. Изменена механика и интерфейс кнопки back-to-top

--- src/js/back-to-top.js
+++ src/js/back-to-top.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
   var toTopTimer;
   console.log('ghjf');
   $(window).on('scroll', function(){
-    if ($(window).scrollTop() >= 400){
-      toTop.addClass('back-to-top_active');
+    if ($(window).scrollTop() >= 100){
+      toTop.addClass('top-btn__back-to-top_active');
     } else {
-      toTop.removeClass('back-to-top_active');
-      toTop.removeClass('back-to-top_focus');
+      toTop.removeClass('top-btn__back-to-top_active');
+      toTop.removeClass('top-btn__back-to-top_focus');
 
     }
   });
   toTop.on('click',function(){
-    toTop.addClass('back-to-top_focus');
+    toTop.addClass('top-btn__back-to-top_focus');
     $('html, body').animate({scrollTop:0},1000);
     
   });  
--- src/js/modal.js
+++ src/js/modal.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 
   button.on('click', function(){
     modal.addClass('modal_active');
+   // modalTimer = setTimeout( modal.removeClass('modal_active'), 5000);
   });
   close.on('click',function(){
     modal.removeClass('modal_active');
